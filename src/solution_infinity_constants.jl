7886f72a8d2f5cf940d1263f319aba87984752f0
Joel Dahne <joel@dahne.eu>
2023-06-09 19:34:59 +0200

    Add new methods C_fix_point_dγ and solution_infinity_γ

--- src/solution_infinity_constants.jl
+++ src/solution_infinity_constants.jl
@@ -449,7 +449,7 @@ real(c) = κ * ϵ / (1 + ϵ)^2
 # Splitting `T`
 Since `u` is a fix point of `T` we have
 ```
-u(ξ) = γ * P(ξ) - (1 + im * δ) ∫_ξ₁^∞ K(ξ, η) * abs(u(η))^2σ * u(η) dη
+u(ξ) = γ * P(ξ) - (1 + im * δ) * ∫_ξ₁^∞ K(ξ, η) * abs(u(η))^2σ * u(η) dη
 ```
 If we let
 ```
@@ -617,7 +617,7 @@ function C_fix_point(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁
 end
 
 """
-    C_fix_point_derivative(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
+    C_fix_point_dξ(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
 
 Return `C₃` and `C₄` such that the fix point `u(ξ)` of the operator
 `T` satisfies
@@ -627,7 +627,7 @@ u'(ξ) = γ * P'(ξ) + P'(ξ) * f(ξ) + P(ξ) * f'(ξ) + E'(ξ) * g(ξ) + E(ξ) 
 with
 ```
 abs(f'(ξ)) <= C₃ * ξ^(2σ * v + v - 3)
-abs(g'(ξ)) <= C₄ * exp(-real(c) * ξ^2) * (-2 / σ + 2σ * v + v + d - 3)
+abs(g'(ξ)) <= C₄ * exp(-real(c) * ξ^2) * ξ^(-2 / σ + 2σ * v + v + d - 3)
 ```
 and `abs(f(ξ))` and `abs(g(ξ))` bounded as in [`C_fix_point`](@ref).
 This holds for for `ξ >= ξ₁` and `abs(γ) <= r₁`. In particular for `ξ
@@ -704,7 +704,7 @@ C₄ = abs(1 + im * δ) / abs(1 - im * ϵ) * CP / abs(2c) *
 # Bounding `norm(u, v)`
 This is done as in [`C_fix_point`](@ref).
 """
-function C_fix_point_derivative(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
+function C_fix_point_dξ(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
     d, ω, σ, ϵ, δ = p.d, p.ω, p.σ, p.ϵ, p.δ
 
     a = Acb(1 / σ, ω / κ) / 2
@@ -752,4 +752,175 @@ function C_fix_point_derivative(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{
         ρ^(2σ + 1)
 
     return C₃, C₄
+end
+
+"""
+    C_fix_point_dγ(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
+
+Return `C` such that the fix point `u(ξ)` of the operator
+`T` satisfies
+```
+diff(u(ξ), γ) = P(ξ) + h(ξ)
+```
+with
+```
+abs(h(ξ)) <= C * ξ^XXX
+```
+Here we use `diff(u(ξ₁), γ)` to denote the derivative of `u(ξ₁)` with
+respect to `γ`. This holds for `ξ >= ξ₁` and `abs(γ) <= r₁`.
+
+# Splitting `T`
+Since `u` is a fix point of `T` we have
+```
+u(ξ) = γ * P(ξ) - (1 + im * δ) * ∫_ξ₁^∞ K(ξ, η) * abs(u(η))^2σ * u(η) dη
+```
+Differentiating with respect to `γ` we get
+```
+diff(u(ξ), γ) = P(ξ) - (1 + im * δ) * ∫_ξ₁^∞ K(ξ, η) * diff(abs(u(η))^2σ * u(η), γ) dη
+```
+We let
+```
+h(ξ) = (1 + im * δ) * ∫_ξ₁^∞ K(ξ, η) * diff(abs(u(η))^2σ * u(η), γ) dη
+```
+
+# Simplifying `diff(abs(u(η))^2σ * u(η), γ)`
+We have
+```
+diff(abs(u(η))^2σ * u(η), γ) = diff(abs(u(η))^2σ, γ) * u(η) + abs(u(η))^2σ * diff(u(η), γ)
+```
+Furthermore if we let `u(η) = x(η) + im * y(η)` we have
+```
+diff(abs(u(η))^2σ, γ) = diff((x(η)^2 + y(η)^2)^σ, γ)
+    = σ * (x(η)^2 + y(η)^2)^(σ - 1) * diff(x(η)^2 + y(η)^2, γ)
+    = 2σ * abs(u(η))^(2σ - 2) * (x(η) * diff(x(η), γ) + y(η) * diff(y, γ))
+```
+If note that
+```
+conj(u(η)) * diff(u(η), γ) + u(η) * conj(diff(u(η), γ)) = x(η) * diff(x(η), γ) + y(η) * diff(y, γ)
+```
+Hence
+```
+diff(abs(u(η))^2σ, γ) = 2σ * abs(u(η))^(2σ - 2) * (
+    conj(u(η)) * diff(u(η), γ) + u(η) * conj(diff(u(η), γ))
+)
+```
+This gives us
+```
+diff(abs(u(η))^2σ * u(η), γ) =
+    2σ * abs(u(η))^(2σ - 2) * conj(u(η)) * diff(u(η), γ) +
+    2σ * abs(u(η))^(2σ - 2) * u(η) * conj(diff(u(η), γ)) +
+    abs(u(η))^2σ * diff(u(η), γ)
+```
+
+# Bounding `abs(h(ξ))`
+We have
+```
+abs(h(ξ)) <= abs(1 + im * δ) * ∫_ξ₁^∞ abs(K(ξ, η)) * abs(diff(abs(u(η))^2σ * u(η), γ)) dη
+```
+With the expression for `diff(abs(u(η))^2σ * u(η), γ)` from above we
+get
+```
+abs(diff(abs(u(η))^2σ * u(η), γ)) <=
+    2σ * abs(u(η))^(2σ - 1) * abs(diff(u(η), γ)) +
+    2σ * abs(u(η))^(2σ - 1) * abs(diff(u(η), γ)) +
+    abs(u(η))^2σ * abs(diff(u(η), γ))
+= abs(u(η))^(2σ - 1) * (2σ + abs(u(η))) * abs(diff(u(η), γ))
+```
+This gives us
+```
+abs(h(ξ)) <= abs(1 + im * δ) * ∫_ξ₁^∞ abs(K(ξ, η)) *
+    abs(u(η))^(2σ - 1) * (2σ + abs(u(η))) * abs(diff(u(η), γ)) dη
+```
+Using that
+```
+abs(u(η)) <= norm(u, v) * η^(v - 1 / σ)
+abs(diff(u(η), γ)) <= norm(diff(u, γ), v) * η^(v - 1 / σ)
+```
+we get
+```
+abs(h(ξ)) <= abs(1 + im * δ) * norm(u, v)^(2σ - 1) * norm(diff(u, γ), v) *
+    ∫_ξ₁^∞ abs(K(ξ, η)) * (2σ + norm(u, v) * η^(v - 1 / σ)) * η^((v - 1 / σ) * 2σ) dη
+```
+
+"""
+function C_fix_point_dγ(r₁::Arb, v::Arb, κ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb)
+    d, ω, σ, ϵ, δ = p.d, p.ω, p.σ, p.ϵ, p.δ
+    q = 2
+    a = Acb(1 / σ, ω / κ) / 2
+    b = Arb(d // 2)
+    c = Acb(0, -κ) / 2Acb(1, -ϵ)
+
+    CP = C_P(κ, p, ξ₁)
+    CE = C_E(κ, p, ξ₁)
+
+    # TODO
+    C = one(CP)
+
+    return C
+end
+
+"""
+    solution_infinity_γ(κ::Arb, μ::Arb, p::AbstractGLParams{Arb}, ξ₁::Arb, v::Arb; u_0::Acb)
+
+Compute enclosure of `γ` occurring in the fixed point equation. If the
+fixed point cannot be proved to exist then returns an indeterminate
+value.
+
+**IMPROVE:** Add more documentation
+"""
+function solution_infinity_γ(
+    κ::Arb,
+    μ::Arb,
+    p::AbstractGLParams{Arb},
+    ξ₁::Arb,
+    v::Arb,
+    u_0::Acb,
+)
+    # Compute approximate γ
+    γ₀ = u_0 / P(ξ₁, (p, κ))
+
+    # Enclose u_T(ξ₁, (p, κ, γ₀))
+    u_T = let
+        _, C₂ = C_fix_point(abs(γ₀), v, κ, p, ξ₁)
+
+        real_c = κ * p.ϵ / (1 + p.ϵ)^2
+
+        g_bound = add_error(
+            zero(C₂),
+            C₂ * exp(-real_c * ξ₁^2) * ξ₁^(-2 / p.σ + 2p.σ * v + v + p.d - 2),
+        )
+
+        γ₀ * P(ξ₁, (p, κ)) + E(ξ₁, (p, κ)) * g_bound
+    end
+
+    # TODO: Choose the radius dynamically
+    γ_ball = add_error(γ₀, Mag(1e-3))
+
+    u_T_dγ = let
+        C₃ = C_fix_point_dγ(abs(γ_ball), v, κ, p, ξ₁)
+
+        # FIXME: This is not the correct expression
+        real_c = κ * p.ϵ / (1 + p.ϵ)^2
+
+        h_bound = add_error(
+            zero(C₃),
+            C₃ *
+            abs(E(ξ₁, (p, κ))) *
+            exp(-real_c * ξ₁^2) *
+            ξ₁^(-2 / p.σ + 2p.σ * v + v + p.d - 2),
+        )
+
+        P(ξ₁, (p, κ)) + h_bound
+    end
+
+    # Perform one Newton iteration
+    γ_ball_new = γ₀ - (u_T - u_0) / u_T_dγ
+
+    success = Arblib.contains_interior(γ_ball, γ_ball_new)
+
+    if success
+        return γ_ball_new
+    else
+        return indeterminate(γ_ball_new)
+    end
 end
