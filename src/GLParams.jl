97d6fd7084ff818d2c73763f0d8f205eee8cae5f
Joel Dahne <joel@dahne.eu>
2023-09-08 16:10:08 +0200

    Add _params method for returning common parameters

--- src/GLParams.jl
+++ src/GLParams.jl
@@ -54,3 +54,36 @@ end
 
 gl_params(p::AbstractGLParams) = gl_params(p.d, p.ω, p.σ, p.ϵ, p.δ)
 gl_params(T::Type, p::AbstractGLParams) = gl_params(T, p.d, p.ω, p.σ, p.ϵ, p.δ)
+
+function _params(T::Type{Float64}, i::Integer = 1, d::Integer = 1)
+    if d == 1
+        λ = gl_params(1, 1.0, 2.3, 0.0, 0.0)
+
+        μs = [1.23204, 0.78308, 1.12389, 0.88393, 1.07969, 0.92761, 1.05707, 0.94914]
+        κs = [0.85310, 0.49322, 0.34680, 0.26678, 0.21621, 0.18192, 0.15667, 0.13749]
+
+    elseif d == 3
+        λ = gl_params(T, 3, 1.0, 1.0, 0.0, 0.0)
+
+        μs = [1.88619, 0.84142, 1.10919, 0.94337, 1.01123]
+        κs = [0.91710, 0.32129, 0.22259, 0.16961, 0.13738]
+    else
+        error("only contains values d = 1 or d = 3")
+    end
+
+    ξ₁ = 30.0
+
+    μ, γ, κ = approximate_parameters(μs[i], κs[i], ξ₁, λ)
+
+    return μ, γ, κ, ξ₁, λ
+end
+
+function _params(T::Type, i::Integer = 1, d::Integer = 1)
+    μ, γ, κ, ξ₁, λ = _params(Float64, i, d)
+
+    if T == Arb
+        return T(μ), Acb(γ), T(κ), T(ξ₁), gl_params(T, λ)
+    else
+        return T(μ), complex(T)(γ), T(κ), T(ξ₁), gl_params(T, λ)
+    end
+end
