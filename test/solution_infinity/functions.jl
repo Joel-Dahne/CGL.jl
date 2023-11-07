0956c0ba7ca955b7b033aed78098283b421162f9
Joel Dahne <joel@dahne.eu>
2023-11-07 13:27:43 +0100

    Add more tests for functions

--- test/solution_infinity/functions.jl
+++ test/solution_infinity/functions.jl
@@ -4,6 +4,14 @@
     λ = gl_params(Arb, 1, 1.0, 2.3, 0.1, 0.2)
     _, _, c = CGL._abc(κ, λ)
 
+    ξF64 = Float64(ξ)
+    κF64 = Float64(κ)
+    λF64 = gl_params(Float64, λ)
+
+    # Function for computing derivative using finite differences.
+    fdm = central_fdm(5, 1)
+    fdm2 = central_fdm(5, 2)
+
     @testset "P" begin
         @test Arblib.overlaps(P(ArbSeries((ξ, 1)), (λ, κ))[1], P_dξ(ξ, (λ, κ)))
 
@@ -12,12 +20,32 @@
         @test CGL.P_asym_approx(ξ, (λ, κ)) ≈ P(ξ, (λ, κ)) rtol = 1e-20
 
         @test CGL.P_dξ_asym_approx(ξ, (λ, κ)) ≈ P_dξ(ξ, (λ, κ)) rtol = 1e-20
+
+        @test P_dξ(ξ, (λ, κ)) ≈ fdm(ξ -> P(ξ, (λF64, κF64)), ξF64) rtol = 1e-12
+
+        @test P_dξ_dξ(ξ, (λ, κ)) ≈ fdm2(ξ -> P(ξ, (λF64, κF64)), ξF64) rtol = 1e-10
+
+        @test P_dκ(ξ, (λ, κ)) ≈ fdm(κ -> P(ξF64, (λF64, κ)), κF64) rtol = 1e-12
+
+        @test P_dξ_dκ(ξ, (λ, κ)) ≈ fdm(κ -> P_dξ(ξF64, (λF64, κ)), κF64) rtol = 1e-12
     end
 
     @testset "E" begin
         @test Arblib.overlaps(E(ArbSeries((ξ, 1)), (λ, κ))[1], E_dξ(ξ, (λ, κ)))
 
         @test Arblib.overlaps(E(ξ, (λ, ArbSeries((κ, 1))))[1], E_dκ(ξ, (λ, κ)))
+
+        @test E_dξ(ξ, (λ, κ)) ≈ fdm(ξ -> E(ξ, (λF64, κF64)), ξF64) rtol = 1e-10
+
+        @test E_dξ_dξ(ξ, (λ, κ)) ≈ fdm2(ξ -> E(ξ, (λF64, κF64)), ξF64) rtol = 1e-6
+
+        @test E_dκ(ξ, (λ, κ)) ≈ fdm(κ -> E(ξF64, (λF64, κ)), κF64) rtol = 1e-10
+
+        @test E_dξ_dκ(ξ, (λ, κ)) ≈ fdm(κ -> E_dξ(ξF64, (λF64, κ)), κF64) rtol = 1e-10
+    end
+
+    @testset "B_W" begin
+        @test CGL.B_W_dκ(κ, λ) ≈ fdm(κ -> CGL.B_W(κ, λF64), κF64) rtol = 1e-12
     end
 
     @testset "J_P" begin
@@ -31,6 +59,10 @@
             E(ξ, (λ, κ)) * J_P(η, (λ, κ)),
             -(1 + im * λ.δ) * K(ξ, η, (λ, κ)),
         )
+
+        @test J_P_dξ(ξ, (λ, κ)) ≈ fdm(ξ -> J_P(ξ, (λF64, κF64)), ξF64) rtol = 1e-10
+
+        @test J_P_dκ(ξ, (λ, κ)) ≈ fdm(κ -> J_P(ξF64, (λF64, κ)), κF64) rtol = 1e-10
     end
 
     @testset "J_E" begin
@@ -44,5 +76,9 @@
             P(ξ, (λ, κ)) * J_E(η, (λ, κ)),
             -(1 + im * λ.δ) * K(ξ, η, (λ, κ)),
         )
+
+        @test J_E_dξ(ξ, (λ, κ)) ≈ fdm(ξ -> J_E(ξ, (λF64, κF64)), ξF64) rtol = 1e-10
+
+        @test J_E_dκ(ξ, (λ, κ)) ≈ fdm(κ -> J_E(ξF64, (λF64, κ)), κF64) rtol = 1e-10
     end
 end
