59232663d36247d8e41c0856b579636c63663bc5
Joel Dahne <joel@dahne.eu>
2023-11-13 09:43:06 +0100

    Update tolerance in tests

--- test/approximate_parameters.jl
+++ test/approximate_parameters.jl
@@ -22,9 +22,11 @@
         # Check the full version
         μ2, γ2, κ2 = CGL.approximate_parameters(μ, γ, κ, ξ₁, λ)
 
-        @test μ ≈ μ2
-        @test γ ≈ γ2
-        @test κ ≈ κ2
+        # The full version uses a higher order approximation and the
+        # result can differ quite a lot.
+        @test μ ≈ μ2 rtol = 1e-3
+        @test γ ≈ γ2 rtol = 1e-3
+        @test κ ≈ κ2 rtol = 1e-3
 
         @test CGL.G_real(μ2, real(γ2), imag(γ2), κ2, ξ₁, λ) ≈ [0, 0, 0, 0] atol = 1e-12
     end
