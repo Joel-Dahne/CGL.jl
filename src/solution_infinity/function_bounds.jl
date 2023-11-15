ce1997f87032c791246ee667cd984d8b5fe171b0
Joel Dahne <joel@dahne.eu>
2023-11-15 11:03:54 +0100

    Simplify some function bounds

--- src/solution_infinity/function_bounds.jl
+++ src/solution_infinity/function_bounds.jl
@@ -11,6 +11,7 @@ It makes use of the asymptotic expansion given by
 https://fungrim.org/entry/d1b3b5/ together with error bounds for the
 remainder term from https://fungrim.org/entry/461a54/.
 
+# Extended help
 The asymptotic expansion with `n` terms is given by
 ```
 z^-a * sum(0:n-1) do k
@@ -72,118 +73,28 @@ function C_hypgeom_u(a::Acb, b::Acb, z₁::Acb, n::Integer = 5)
     return C
 end
 
-"""
-    C_hypgeom_u_dz(a::Acb, b::Acb, z₁::Acb)
+C_hypgeom_u_dz(a::Acb, b::Acb, z₁::Acb) = abs(a) * C_hypgeom_u(a + 1, b + 1, z₁)
 
-Return `C` such that
-```
-abs(hypgeom_u_dz(a, b, z)) <= C * abs(z^(-a - 1))
-```
-for `z` such that `abs(imag(z)) > abs(imag(z₁))` and `abs(z) > abs(z₁)`
-
-Uses that
-```
-hypgeom_u_dz(a, b, z) = -hypgeom_u(a + 1, b + 1, z) * a
-```
-"""
-C_hypgeom_u_dz(a::Acb, b::Acb, z₁::Acb, n::Integer = 5) =
-    abs(a) * C_hypgeom_u(a + 1, b + 1, z₁, n)
-
-"""
-    C_P(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
-
-Return `C` such that
-```
-abs(P(ξ, (p, κ))) <= C * ξ^(-1 / σ)
-```
-for `ξ >= ξ₁ > 0`.
-
-Let
-```
-a = (1 / σ + im * ω / κ) / 2
-b = d / 2
-c = -im * κ / 2(1 - im * ϵ)
-```
-We then have
-```
-P(ξ) = hypgeom_u(a, b, c * ξ^2)
-```
-
-If we let `CU = C_hypgeom_u(a, b, c * ξ₁)` then we have for `ξ` such
-that `abs(imag(c * ξ)) > abs(imag(c * ξ₁))` and `abs(c * ξ₁) > abs(c *
-ξ₁)` that
-```
-abs(hypgeom_u(a, b, c * ξ)) <= CU * abs((c * ξ)^-a)
-    = CU * abs(c^-a) * abs(ξ^-a)
-    = CU * abs(c^-a) * ξ^(-1 / σ)
-```
-In particular we have that if `ξ >= ξ₁` then both the requirements are
-satisfied. We can thus take
-```
-C = CU * abs(c^-a)
-```
-"""
 function C_P(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
     ξ₁ > 0 || throw(ArgumentError("assuming ξ₁ > 0"))
 
     a, b, c = _abc(κ, λ)
 
-    C = C_hypgeom_u(a, b, c * ξ₁^2) * abs(c^-a)
-
-    return C
+    return C_hypgeom_u(a, b, c * ξ₁^2) * abs(c^-a)
 end
 
-"""
-    C_E(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
-
-Return `C` such that
-```
-abs(E(ξ, (p, κ))) <= C * exp(real(c) * ξ^2) * ξ^(1 / σ - d)
-```
-for `ξ >= ξ₁ > 0`, with `c` as below. Note that if `ϵ = 0` then
-`real(z) = 0` and the exponential term can be removed.
-
-Se also [`C_P`](@ref).
-
-Let
-```
-a = (1 / σ + im * ω / κ) / 2
-b = d / 2
-c = -im * κ / 2(1 - im * ϵ)
-```
-We then have
-```
-E(ξ) = exp(c * ξ^2) * hypgeom_u(b - a, b, -c * ξ^2)
-```
-
-If we let `CU = C_hypgeom_u(b - a, b, -c * ξ₁^2)` then we have for `ξ`
-such that `abs(imag(c * ξ)) > abs(imag(c * ξ₁))` and `abs(c * ξ₁) >
-abs(c * ξ₁)` that
-```
-abs(hypgeom_u(b - a, b, -c * ξ^2)) <= CU * abs((-c * ξ^2)^(a - b))
-    = CU * abs((-c)^(a - b)) * abs(ξ^(2(a - b)))
-    = CU * abs((-c)^(a - b)) * ξ^(1 / σ - d)
-```
-In particular we have that if `ξ >= ξ₁` then `z` satisfies both the
-requirements. If we let `C = CU * abs((-c)^(a - b))` and use that
-`abs(exp(c * ξ^2)) = exp(real(c) * ξ^2)` we thus get
-```
-E(ξ) <= C * exp(real(c) * ξ^2) * ξ^(1 / σ - d)
-```
-as required.
-"""
 function C_E(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
     ξ₁ > 0 || throw(ArgumentError("assuming ξ₁ > 0"))
 
     a, b, c = _abc(κ, λ)
 
-    C = C_hypgeom_u(b - a, b, -c * ξ₁^2) * abs((-c)^(a - b))
-
-    return C
+    return C_hypgeom_u(b - a, b, -c * ξ₁^2) * abs((-c)^(a - b))
 end
 
 # TODO
 function C_P_dξ(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
+    ξ₁ > 0 || throw(ArgumentError("assuming ξ₁ > 0"))
+
     f = ξ -> ξ^(-1 / λ.σ - 1)
 
     # FIXME: This is only an approximation. It seems to be good
@@ -358,49 +269,10 @@ function C_K(κ::Arb, λ::AbstractGLParams{Arb}, ξ₁::Arb)
     return C
 end
 
-"""
-    C_J_P(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb})
+C_J_P(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb}) = abs(B_W(κ, λ)) * C_P(κ, λ, ξ₁)
 
-Return `C` such that for
-```
-J_P(ξ) = -(1 + im * δ) / (1 - im * ϵ) * P(ξ) * inv(W(ξ))
-```
-we have
-```
-abs(J_P(ξ)) <= C * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d - 1)
-```
-for `ξ >= ξ₁`.
-"""
-function C_J_P(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb})
-    _, _, c = _abc(κ, λ)
+C_J_E(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb}) = abs(B_W(κ, λ)) * C_E(κ, λ, ξ₁)
 
-    f = ξ -> exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d - 1)
-
-    # FIXME: This is only an approximation
-    return 1.01 * abs(J_P(ξ₁, (λ, κ))) / f(ξ₁)
-end
-
-"""
-    C_J_E(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb})
-
-Return `C` such that for
-```
-J_E(ξ) = -(1 + im * δ) / (1 - im * ϵ) * E(ξ) * inv(W(ξ))
-```
-we have
-```
-abs(J_E(ξ)) <= C * ξ^(1 / σ - 1)
-```
-for `ξ >= ξ₁`.
-"""
-function C_J_E(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb})
-    f = ξ -> ξ^(1 / λ.σ - 1)
-
-    # FIXME: This is only an approximation
-    return 1.01 * abs(J_E(ξ₁, (λ, κ))) / f(ξ₁)
-end
-
-# TODO
 function C_J_P_dξ(κ::Arb, ξ₁::Arb, λ::AbstractGLParams{Arb})
     _, _, c = _abc(κ, λ)
 
