74352947c256c95687438be6a426a81513a81e8c
Joel Dahne <joel@dahne.eu>
2023-12-23 20:40:42 +0100

    refine_approximation: Add support for Arb input and output

--- src/refine_approximation.jl
+++ src/refine_approximation.jl
@@ -13,12 +13,6 @@ initial approximation
 ```
 for it.
 """
-function refine_approximation(μ₀::Float64, κ₀::Float64, ξ₁::Float64, λ::CGLParams{Float64})
-    γ₀ = solution_zero(μ₀, κ₀, ξ₁, λ)[1] / P(ξ₁, (λ, κ₀))
-
-    return refine_approximation(μ₀, γ₀, κ₀, ξ₁, λ)
-end
-
 function refine_approximation(
     μ₀::Float64,
     γ₀::ComplexF64,
@@ -33,4 +27,29 @@ function refine_approximation(
     μ, γ_real, γ_imag, κ = sol.zero
 
     return μ, complex(γ_real, γ_imag), κ
+end
+
+function refine_approximation(μ₀::Arb, γ₀::Acb, κ₀::Arb, ξ₁::Arb, λ::CGLParams{Arb})
+    μ, γ, κ = refine_approximation(
+        Float64(μ₀),
+        ComplexF64(γ₀),
+        Float64(κ₀),
+        Float64(ξ₁),
+        CGLParams{Float64}(λ),
+    )
+
+    return Arb(μ), Acb(γ), Arb(κ)
+end
+
+function refine_approximation(μ₀::Float64, κ₀::Float64, ξ₁::Float64, λ::CGLParams{Float64})
+    γ₀ = solution_zero(μ₀, κ₀, ξ₁, λ)[1] / P(ξ₁, (λ, κ₀))
+
+    return refine_approximation(μ₀, γ₀, κ₀, ξ₁, λ)
+end
+
+function refine_approximation(μ₀::Arb, κ₀::Arb, ξ₁::Arb, λ::CGLParams{Arb})
+    μ, γ, κ =
+        refine_approximation(Float64(μ₀), Float64(κ₀), Float64(ξ₁), CGLParams{Float64}(λ))
+
+    return Arb(μ), Acb(γ), Arb(κ)
 end
