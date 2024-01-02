07e97c628bf04f5318a0d50aebb0a74b80368aa9
Joel Dahne <joel@dahne.eu>
2024-01-02 13:20:46 +0100

    Fix tests for function bounds

--- test/solution_infinity/function_bounds.jl
+++ test/solution_infinity/function_bounds.jl
@@ -7,372 +7,136 @@
 
     ξ₁ = Arb(30)
 
-    @testset "C_U" begin
-        for (κ, λ) in params
-            a, b, c = CGL._abc(κ, λ)
-            z₁ = c * ξ₁^2
-            C = CGL.C_U(a, b, z₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                z = k * z₁
-                @test abs(U(a, b, z)) <= C * abs(z^(-a))
-                @test abs(U(a, b, z)) >= 0.9C * abs(z^(-a))
-            end
+    @testset "U $i" for (i, (κ, λ)) in enumerate(params)
+        (; d, σ) = λ
+        a, b, c = CGL._abc(κ, λ)
+        z₁ = c * ξ₁^2
+
+        CU = CGL.C_U(a, b, z₁)
+        CU_dz = CGL.C_U_dz(a, b, z₁)
+        CU_da = CGL.C_U_da(a, b, z₁)
+
+        for z in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64] .* z₁
+            @test abs(U(a, b, z)) <= CU * abs(z^(-a))
+            @test abs(U(a, b, z)) >= 0.9CU * abs(z^(-a))
+
+            @test abs(U_dz(a, b, z)) <= CU_dz * abs(z^(-a - 1))
+            @test abs(U_dz(a, b, z)) >= 0.9CU_dz * abs(z^(-a - 1))
+
+            @test abs(U_da(a, b, z)) <= CU_da * abs(log(z) * z^(-a))
+            @test abs(U_da(a, b, z)) >= 0.9CU_da * abs(log(z) * z^(-a))
         end
     end
 
-    @testset "C_U_dz" begin
-        for (κ, λ) in params
-            a, b, c = CGL._abc(κ, λ)
-            z₁ = c * ξ₁^2
+    @testset "FunctionBounds $i" for (i, (κ, λ)) in enumerate(params)
+        (; d, σ) = λ
+        _, _, c = CGL._abc(κ, λ)
 
-            C = CGL.C_U_dz(a, b, z₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                z = k * z₁
-                @test abs(U_dz(a, b, z)) <= C * abs(z^(-a - 1))
-                @test abs(U_dz(a, b, z)) >= 0.9C * abs(z^(-a - 1))
-            end
-        end
-    end
+        C = CGL.FunctionBounds(κ, ξ₁, λ)
 
-    @testset "C_U_da" begin
-        for (κ, λ) in params
-            a, b, c = CGL._abc(κ, λ)
-            z₁ = c * ξ₁^2
-            C = CGL.C_U_da(a, b, z₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                z = k * z₁
-                @test abs(U_da(a, b, z)) <= C * abs(log(z) * z^(-a))
-                @test abs(U_da(a, b, z)) >= 0.9C * abs(log(z) * z^(-a))
-            end
-        end
-    end
+        for ξ in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64] .* ξ₁
+            ####
+            ## P
+            ####
+            @test abs(P(ξ, (λ, κ))) <= C.P * ξ^(-1 / σ)
+            @test abs(P(ξ, (λ, κ))) >= 0.9C.P * ξ^(-1 / σ)
 
-    @testset "C_P" begin
-        for (κ, λ) in params
-            C = CGL.C_P(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(P(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ)
-                @test abs(P(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ)
-            end
-        end
-    end
+            @test abs(P_dξ(ξ, (λ, κ))) <= C.P_dξ * ξ^(-1 / σ - 1)
+            @test abs(P_dξ(ξ, (λ, κ))) >= 0.9C.P_dξ * ξ^(-1 / σ - 1)
 
-    @testset "C_E" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_E(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(CGL.E(ξ, (λ, κ))) <= C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d)
-                @test abs(CGL.E(ξ, (λ, κ))) >= 0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d)
-            end
-        end
-    end
+            @test abs(P_dξ_dξ(ξ, (λ, κ))) <= C.P_dξ_dξ * ξ^(-1 / σ - 2)
+            @test abs(P_dξ_dξ(ξ, (λ, κ))) >= 0.9C.P_dξ_dξ * ξ^(-1 / σ - 2)
 
-    @testset "C_P_dξ" begin
-        for (κ, λ) in params
-            C = CGL.C_P_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(P_dξ(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ - 1)
-                @test abs(P_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ - 1)
-            end
-        end
-    end
+            @test abs(P_dξ_dξ_dξ(ξ, (λ, κ))) <= C.P_dξ_dξ_dξ * ξ^(-1 / σ - 3)
+            @test abs(P_dξ_dξ_dξ(ξ, (λ, κ))) >= 0.9C.P_dξ_dξ_dξ * ξ^(-1 / σ - 3)
 
-    @testset "C_E_dξ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_E_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(E_dξ(ξ, (λ, κ))) <= C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 1)
-                @test abs(E_dξ(ξ, (λ, κ))) >=
-                      0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 1)
-            end
-        end
-    end
+            # IMPROVE: The three below ones don't give very tight bounds
 
-    @testset "C_P_dξ_dξ" begin
-        for (κ, λ) in params
-            C = CGL.C_P_dξ_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(P_dξ_dξ(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ - 2)
-                @test abs(P_dξ_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ - 2)
-            end
-        end
-    end
+            @test abs(P_dκ(ξ, (λ, κ))) <= C.P_dκ * log(ξ) * ξ^(-1 / σ)
+            @test abs(P_dκ(ξ, (λ, κ))) >= 0.4C.P_dκ * log(ξ) * ξ^(-1 / σ)
 
-    @testset "C_P_dξ_dξ_dξ" begin
-        for (κ, λ) in params
-            C = CGL.C_P_dξ_dξ_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(P_dξ_dξ_dξ(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ - 3)
-                @test abs(P_dξ_dξ_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ - 3)
-            end
-        end
-    end
+            @test abs(P_dξ_dκ(ξ, (λ, κ))) <= C.P_dξ_dκ * log(ξ) * ξ^(-1 / σ - 1)
+            @test abs(P_dξ_dκ(ξ, (λ, κ))) >= 0.25C.P_dξ_dκ * log(ξ) * ξ^(-1 / σ - 1)
 
-    @testset "C_E_dξ_dξ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_E_dξ_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(E_dξ_dξ(ξ, (λ, κ))) <=
-                      C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 2)
-                @test abs(E_dξ_dξ(ξ, (λ, κ))) >=
-                      0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 2)
-            end
-        end
-    end
+            @test abs(P_dξ_dξ_dκ(ξ, (λ, κ))) <= C.P_dξ_dξ_dκ * log(ξ) * ξ^(-1 / σ - 2)
+            @test abs(P_dξ_dξ_dκ(ξ, (λ, κ))) >= 0.1C.P_dξ_dξ_dκ * log(ξ) * ξ^(-1 / σ - 2)
 
-    @testset "C_E_dξ_dξ_dξ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_E_dξ_dξ_dξ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(E_dξ_dξ_dξ(ξ, (λ, κ))) <=
-                      C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 3)
-                @test abs(E_dξ_dξ_dξ(ξ, (λ, κ))) >=
-                      0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 3)
-            end
-        end
-    end
+            ####
+            ## E
+            ####
+            @test abs(CGL.E(ξ, (λ, κ))) <= C.E * exp(real(c * ξ^2)) * ξ^(1 / σ - d)
+            @test abs(CGL.E(ξ, (λ, κ))) >= 0.9C.E * exp(real(c * ξ^2)) * ξ^(1 / σ - d)
 
-    @testset "C_P_dκ" begin
-        for (κ, λ) in params
-            C = CGL.C_P_dκ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(P_dκ(ξ, (λ, κ))) <= C * log(ξ) * ξ^(-1 / λ.σ)
-                # IMPROVE: This doesn't give a very tight upper bound
-                @test abs(P_dκ(ξ, (λ, κ))) >= 0.4C * log(ξ) * ξ^(-1 / λ.σ)
-            end
-        end
-    end
+            @test abs(E_dξ(ξ, (λ, κ))) <= C.E_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 1)
+            @test abs(E_dξ(ξ, (λ, κ))) >= 0.9C.E_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 1)
 
-    @testset "C_E_dκ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_E_dκ(κ, λ, ξ₁)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(E_dκ(ξ, (λ, κ))) <= C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 2)
-                @test abs(E_dκ(ξ, (λ, κ))) >=
-                      0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 2)
-            end
-        end
-    end
+            @test abs(E_dξ_dξ(ξ, (λ, κ))) <=
+                  C.E_dξ_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 2)
+            @test abs(E_dξ_dξ(ξ, (λ, κ))) >=
+                  0.9C.E_dξ_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 2)
 
-    @testset "C_P_dξ_dκ" begin
-        # The implementation of C_P_dξ_dκ gives terrible bounds when ξ
-        # is to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 2ξ₁
-            for (κ, λ) in params
-                C = CGL.C_P_dξ_dκ(κ, λ, ξ₁)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(P_dξ_dκ(ξ, (λ, κ))) <= C * log(ξ) * ξ^(-1 / λ.σ - 1)
-                    # IMPROVE: This doesn't give a very tight upper bound
-                    @test abs(P_dξ_dκ(ξ, (λ, κ))) >= 0.3C * log(ξ) * ξ^(-1 / λ.σ - 1)
-                end
-            end
-        end
-    end
+            @test abs(E_dξ_dξ_dξ(ξ, (λ, κ))) <=
+                  C.E_dξ_dξ_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 3)
+            @test abs(E_dξ_dξ_dξ(ξ, (λ, κ))) >=
+                  0.9C.E_dξ_dξ_dξ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 3)
 
-    @testset "C_E_dξ_dκ" begin
-        # The implementation of C_E_dξ_dκ gives terrible bounds when ξ
-        # is to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 2ξ₁
-            for (κ, λ) in params
-                _, _, c = CGL._abc(κ, λ)
-                C = CGL.C_E_dξ_dκ(κ, λ, ξ₁)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(E_dξ_dκ(ξ, (λ, κ))) <=
-                          C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 3)
-                    @test abs(E_dξ_dκ(ξ, (λ, κ))) >=
-                          0.9C * exp(real(c * ξ^2)) * ξ^(1 / λ.σ - λ.d + 3)
-                end
-            end
-        end
-    end
+            @test abs(E_dκ(ξ, (λ, κ))) <= C.E_dκ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 2)
+            @test abs(E_dκ(ξ, (λ, κ))) >= 0.9C.E_dκ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 2)
 
-    @testset "C_P_dξ_dξ_dκ" begin
-        # The implementation of C_P_dξ_dκ gives terrible bounds when ξ
-        # is to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 2ξ₁
-            for (κ, λ) in params
-                C = CGL.C_P_dξ_dξ_dκ(κ, λ, ξ₁)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(P_dξ_dξ_dκ(ξ, (λ, κ))) <= C * log(ξ) * ξ^(-1 / λ.σ - 2)
-                    # IMPROVE: This doesn't give a very tight upper bound
-                    @test abs(P_dξ_dξ_dκ(ξ, (λ, κ))) >= 0.1C * log(ξ) * ξ^(-1 / λ.σ - 2)
-                end
-            end
-        end
-    end
+            @test abs(E_dξ_dκ(ξ, (λ, κ))) <=
+                  C.E_dξ_dκ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 3)
+            @test abs(E_dξ_dκ(ξ, (λ, κ))) >=
+                  0.9C.E_dξ_dκ * exp(real(c * ξ^2)) * ξ^(1 / σ - d + 3)
 
-    @testset "C_J_P" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_J_P(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_P(ξ, (λ, κ))) <=
-                      C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d - 1)
-                @test abs(J_P(ξ, (λ, κ))) >=
-                      0.9C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d - 1)
-            end
-        end
-    end
+            ######
+            ## J_P
+            ######
+            @test abs(J_P(ξ, (λ, κ))) <= C.J_P * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d - 1)
+            @test abs(J_P(ξ, (λ, κ))) >= 0.9C.J_P * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d - 1)
 
-    @testset "C_J_E" begin
-        for (κ, λ) in params
-            C = CGL.C_J_E(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_E(ξ, (λ, κ))) <= C * ξ^(1 / λ.σ - 1)
-                @test abs(J_E(ξ, (λ, κ))) >= 0.9C * ξ^(1 / λ.σ - 1)
-            end
-        end
-    end
+            @test abs(J_P_dξ(ξ, (λ, κ))) <= C.J_P_dξ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d)
+            @test abs(J_P_dξ(ξ, (λ, κ))) >=
+                  0.9C.J_P_dξ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d)
 
-    @testset "C_J_P_dξ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_J_P_dξ(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_P_dξ(ξ, (λ, κ))) <= C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d)
-                @test abs(J_P_dξ(ξ, (λ, κ))) >=
-                      0.9C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d)
-            end
-        end
-    end
+            @test abs(J_P_dξ_dξ(ξ, (λ, κ))) <=
+                  C.J_P_dξ_dξ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d + 1)
+            @test abs(J_P_dξ_dξ(ξ, (λ, κ))) >=
+                  0.9C.J_P_dξ_dξ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d + 1)
 
-    @testset "C_J_E_dξ" begin
-        for (κ, λ) in params
-            C = CGL.C_J_E_dξ(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_E_dξ(ξ, (λ, κ))) <= C * ξ^(1 / λ.σ - 2)
-                @test abs(J_E_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(1 / λ.σ - 2)
-            end
-        end
-    end
+            @test abs(J_P_dκ(ξ, (λ, κ))) <=
+                  C.J_P_dκ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d + 1)
+            @test abs(J_P_dκ(ξ, (λ, κ))) >=
+                  0.9C.J_P_dκ * exp(-real(c) * ξ^2) * ξ^(-1 / σ + d + 1)
 
-    @testset "C_J_P_dξ_dξ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_J_P_dξ_dξ(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_P_dξ_dξ(ξ, (λ, κ))) <=
-                      C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d + 1)
-                @test abs(J_P_dξ_dξ(ξ, (λ, κ))) >=
-                      0.9C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d + 1)
-            end
-        end
-    end
+            ######
+            ## J_E
+            ######
+            @test abs(J_E(ξ, (λ, κ))) <= C.J_E * ξ^(1 / σ - 1)
+            @test abs(J_E(ξ, (λ, κ))) >= 0.9C.J_E * ξ^(1 / σ - 1)
 
-    @testset "C_J_E_dξ_dξ" begin
-        for (κ, λ) in params
-            C = CGL.C_J_E_dξ_dξ(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_E_dξ_dξ(ξ, (λ, κ))) <= C * ξ^(1 / λ.σ - 3)
-                @test abs(J_E_dξ_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(1 / λ.σ - 3)
-            end
-        end
-    end
+            @test abs(J_E_dξ(ξ, (λ, κ))) <= C.J_E_dξ * ξ^(1 / σ - 2)
+            @test abs(J_E_dξ(ξ, (λ, κ))) >= 0.9C.J_E_dξ * ξ^(1 / σ - 2)
 
-    @testset "C_J_P_dκ" begin
-        for (κ, λ) in params
-            _, _, c = CGL._abc(κ, λ)
-            C = CGL.C_J_P_dκ(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(J_P_dκ(ξ, (λ, κ))) <=
-                      C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d + 1)
-                @test abs(J_P_dκ(ξ, (λ, κ))) >=
-                      0.9C * exp(-real(c) * ξ^2) * ξ^(-1 / λ.σ + λ.d + 1)
-            end
-        end
-    end
+            @test abs(J_E_dξ_dξ(ξ, (λ, κ))) <= C.J_E_dξ_dξ * ξ^(1 / σ - 3)
+            @test abs(J_E_dξ_dξ(ξ, (λ, κ))) >= 0.9C.J_E_dξ_dξ * ξ^(1 / σ - 3)
 
-    @testset "C_J_E_dκ" begin
-        # The implementation of C_P_dξ_dκ gives terrible bounds when ξ
-        # is to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 2ξ₁
-            for (κ, λ) in params
-                C = CGL.C_J_E_dκ(κ, ξ₁, λ)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(J_E_dκ(ξ, (λ, κ))) <= C * log(ξ) * ξ^(1 / λ.σ - 1)
-                    # IMPROVE: This doesn't give a very tight upper bound
-                    @test abs(J_E_dκ(ξ, (λ, κ))) >= 0.3C * log(ξ) * ξ^(1 / λ.σ - 1)
-                end
-            end
-        end
-    end
+            @test abs(J_E_dκ(ξ, (λ, κ))) <= C.J_E_dκ * log(ξ) * ξ^(1 / σ - 1)
+            # IMPROVE: This doesn't give a very tight bound
+            @test abs(J_E_dκ(ξ, (λ, κ))) >= 0.3C.J_E_dκ * log(ξ) * ξ^(1 / σ - 1)
 
-    @testset "D" begin
-        for (κ, λ) in params
-            C = CGL.C_D(κ, ξ₁, λ)
-            for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                ξ = k * ξ₁
-                @test abs(D(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ)
-                @test abs(D(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ)
-            end
-        end
-    end
+            ####
+            ## D
+            ####
+            @test abs(D(ξ, (λ, κ))) <= C.D * ξ^(-1 / σ)
+            @test abs(D(ξ, (λ, κ))) >= 0.9C.D * ξ^(-1 / σ)
 
-    @testset "D_dξ" begin
-        # The implementation of C_D_dξ gives terrible bounds when ξ is
-        # to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 2ξ₁
-            for (κ, λ) in params
-                C = CGL.C_D_dξ(κ, ξ₁, λ)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(D_dξ(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ - 1)
-                    @test abs(D_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ - 1)
-                end
-            end
-        end
-    end
+            # IMPROVE: The two below ones don't give very tight bounds
 
-    @testset "D_dξ_dξ" begin
-        # The implementation of C_D_dξ_dξ gives terrible bounds when ξ
-        # is to small. We therefore take a large ξ in this case to be
-        # able to check anything. The part giving bad bounds is
-        # _U_da_finite_difference
-        let ξ₁ = 3ξ₁
-            for (κ, λ) in params
-                C = CGL.C_D_dξ_dξ(κ, ξ₁, λ)
-                for k in [1, 1.01, 1.1, 2, 4, 8, 16, 32, 64]
-                    ξ = k * ξ₁
-                    @test abs(D_dξ_dξ(ξ, (λ, κ))) <= C * ξ^(-1 / λ.σ - 2)
-                    @test abs(D_dξ_dξ(ξ, (λ, κ))) >= 0.9C * ξ^(-1 / λ.σ - 2)
-                end
-            end
+            @test abs(D_dξ(ξ, (λ, κ))) <= C.D_dξ * ξ^(-1 / σ - 1)
+            @test abs(D_dξ(ξ, (λ, κ))) >= 0.8C.D_dξ * ξ^(-1 / σ - 1)
+
+            @test abs(D_dξ_dξ(ξ, (λ, κ))) <= C.D_dξ_dξ * ξ^(-1 / σ - 2)
+            @test abs(D_dξ_dξ(ξ, (λ, κ))) >= 0.75C.D_dξ_dξ * ξ^(-1 / σ - 2)
         end
     end
 end
