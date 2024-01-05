29c217fb78a96a03e4f7381e4aec090f3dfdb0f4
Joel Dahne <joel@dahne.eu>
2024-01-05 16:32:04 +0100

    refine_approximation: Add verbose option

--- src/refine_approximation.jl
+++ src/refine_approximation.jl
@@ -18,38 +18,63 @@ function refine_approximation(
     γ₀::ComplexF64,
     κ₀::Float64,
     ξ₁::Float64,
-    λ::CGLParams{Float64},
+    λ::CGLParams{Float64};
+    verbose = false,
 )
     F = x -> G_real(x..., ξ₁, λ)
     # 100 iterations should be enough to saturate the convergence in
     # practice
     sol = nlsolve(F, [μ₀, real(γ₀), imag(γ₀), κ₀], iterations = 100, ftol = 1e-10)
+
+    if verbose && sol.residual_norm > 1e-8
+        @warn "Low precision when refining approximation" λ.ϵ sol.residual_norm
+    end
+
     μ, γ_real, γ_imag, κ = sol.zero
 
     return μ, complex(γ_real, γ_imag), κ
 end
 
-function refine_approximation(μ₀::Arb, γ₀::Acb, κ₀::Arb, ξ₁::Arb, λ::CGLParams{Arb})
+function refine_approximation(
+    μ₀::Arb,
+    γ₀::Acb,
+    κ₀::Arb,
+    ξ₁::Arb,
+    λ::CGLParams{Arb};
+    verbose = false,
+)
     μ, γ, κ = refine_approximation(
         Float64(μ₀),
         ComplexF64(γ₀),
         Float64(κ₀),
         Float64(ξ₁),
-        CGLParams{Float64}(λ),
+        CGLParams{Float64}(λ);
+        verbose,
     )
 
     return Arb(μ), Acb(γ), Arb(κ)
 end
 
-function refine_approximation(μ₀::Float64, κ₀::Float64, ξ₁::Float64, λ::CGLParams{Float64})
+function refine_approximation(
+    μ₀::Float64,
+    κ₀::Float64,
+    ξ₁::Float64,
+    λ::CGLParams{Float64};
+    verbose = false,
+)
     γ₀ = solution_zero(μ₀, κ₀, ξ₁, λ)[1] / P(ξ₁, (λ, κ₀))
 
-    return refine_approximation(μ₀, γ₀, κ₀, ξ₁, λ)
+    return refine_approximation(μ₀, γ₀, κ₀, ξ₁, λ; verbose)
 end
 
-function refine_approximation(μ₀::Arb, κ₀::Arb, ξ₁::Arb, λ::CGLParams{Arb})
-    μ, γ, κ =
-        refine_approximation(Float64(μ₀), Float64(κ₀), Float64(ξ₁), CGLParams{Float64}(λ))
+function refine_approximation(μ₀::Arb, κ₀::Arb, ξ₁::Arb, λ::CGLParams{Arb}; verbose = false)
+    μ, γ, κ = refine_approximation(
+        Float64(μ₀),
+        Float64(κ₀),
+        Float64(ξ₁),
+        CGLParams{Float64}(λ);
+        verbose,
+    )
 
     return Arb(μ), Acb(γ), Arb(κ)
 end
