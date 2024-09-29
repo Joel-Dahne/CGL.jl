9631a4a4b7cb43fae7ed1a42f7889be448801a17
Joel Dahne <joel@dahne.eu>
2024-09-28 21:31:17 -0500

    HPC: Update default values for branch_points with d = 3

--- HPC/scripts/branch_points.jl
+++ HPC/scripts/branch_points.jl
@@ -18,12 +18,20 @@ fix_kappa = length(ARGS) > 1 ? parse(Bool, ARGS[2]) : false
 N = length(ARGS) > 2 ? parse(Int, ARGS[3]) : 0
 scaling = length(ARGS) > 3 ? parse(Float64, ARGS[4]) : 1.0
 
+if d == 1
+    ξ₁_strategy = :default
+    ξ₁_strategy_value = nothing
+else
+    ξ₁_strategy = :automatic
+    ξ₁_strategy_value = [15, 20, 25, 30, 35, 40, 50, 60, 70, 80]
+end
+
 CGL.run_branch_points(
     d;
     fix_kappa,
     scaling,
-    ξ₁_strategy = :default,
-    ξ₁_strategy_value = nothing,
+    ξ₁_strategy,
+    ξ₁_strategy_value,
     N,
     batch_size = num_threads,
     pool,
