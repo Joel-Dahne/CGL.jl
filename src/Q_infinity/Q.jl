234807c5fe66207d1b8c1a80d8d1544f3e0a5826
Joel Dahne <joel@dahne.eu>
2026-08-09 17:03:43 +0200

    Q_infinity: Simplify expression for dQ to match updates in paper

--- src/Q_infinity/Q.jl
+++ src/Q_infinity/Q.jl
@@ -29,11 +29,7 @@ function Q_infinity(γ::Acb, κ::Arb, ϵ::Arb, ξ₁::Arb, Λ::CGLParams{Arb})
         I_P = I_P_enclose(γ, κ, ϵ, ξ₁, v, Q, dQ, Λ, F, C, norms)
 
         Q = γ * F.P + F.E * I_P
-
-        I_E_dξ = F.J_E * abs(Q)^2σ * Q
-        I_P_dξ = -F.J_P * abs(Q)^2σ * Q
-
-        dQ = γ * F.P_dξ + F.P * I_E_dξ + F.E_dξ * I_P + F.E * I_P_dξ
+        dQ = γ * F.P_dξ + F.E_dξ * I_P
     end
 
     return SVector(Q, dQ)
@@ -65,11 +61,7 @@ function Q_infinity(
     I_P = B_W(κ, ϵ, Λ) * exp(-c * ξ₁^2) * p * ξ₁^(d - 2) * abs(Q)^2σ * Q / 2c
 
     Q = γ * p + e * I_P
-
-    I_E_dξ = j_e * abs(Q)^2σ * Q
-    I_P_dξ = -j_p * abs(Q)^2σ * Q
-
-    dQ = γ * p_dξ + p * I_E_dξ + e_dξ * I_P + e * I_P_dξ
+    dQ = γ * p_dξ + e_dξ * I_P
 
     return SVector(Q, dQ)
 end
@@ -115,32 +107,9 @@ function Q_infinity_jacobian_kappa(γ::Acb, κ::Arb, ϵ::Arb, ξ₁::Arb, Λ::CG
         Q_dγ = F.P + F.E * I_P_dγ
         Q_dκ = γ * F.P_dκ + F.E * I_P_dκ + F.E_dκ * I_P
 
-        I_E_dξ = F.J_E * abs(Q)^2σ * Q
-        I_P_dξ = -F.J_P * abs(Q)^2σ * Q
-
-        I_E_dξ_dγ =
-            F.J_E * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dγ) * Q + abs(Q)^2 * Q_dγ)
-        I_P_dξ_dγ =
-            -F.J_P * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dγ) * Q + abs(Q)^2 * Q_dγ)
-
-        I_E_dξ_dκ =
-            F.J_E_dκ * abs(Q)^2σ * Q +
-            F.J_E * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dκ) * Q + abs(Q)^2 * Q_dκ)
-        I_P_dξ_dκ =
-            -F.J_P_dκ * abs(Q)^2σ * Q -
-            F.J_P * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dκ) * Q + abs(Q)^2 * Q_dκ)
-
-
-        dQ = γ * F.P_dξ + F.P * I_E_dξ + F.E_dξ * I_P + F.E * I_P_dξ
-        dQ_dγ = F.P_dξ + F.P * I_E_dξ_dγ + F.E * I_P_dξ_dγ + F.E_dξ * I_P_dγ
-        dQ_dκ =
-            γ * F.P_dξ_dκ +
-            F.P * I_E_dξ_dκ +
-            F.P_dκ * I_E_dξ +
-            F.E * I_P_dξ_dκ +
-            F.E_dξ * I_P_dκ +
-            F.E_dκ * I_P_dξ +
-            F.E_dξ_dκ * I_P
+        dQ = γ * F.P_dξ + F.E_dξ * I_P
+        dQ_dγ = F.P_dξ + F.E_dξ * I_P_dγ
+        dQ_dκ = γ * F.P_dξ_dκ + F.E_dξ * I_P_dκ + F.E_dξ_dκ * I_P
     end
 
     return SMatrix{2,2}(Q_dγ, dQ_dγ, Q_dκ, dQ_dκ)
@@ -204,32 +173,9 @@ function Q_infinity_jacobian_epsilon(γ::Acb, κ::Arb, ϵ::Arb, ξ₁::Arb, Λ::
         Q_dγ = F.P + F.E * I_P_dγ
         Q_dϵ = γ * F.P_dϵ + F.E * I_P_dϵ + F.E_dϵ * I_P
 
-        I_E_dξ = F.J_E * abs(Q)^2σ * Q
-        I_P_dξ = -F.J_P * abs(Q)^2σ * Q
-
-        I_E_dξ_dγ =
-            F.J_E * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dγ) * Q + abs(Q)^2 * Q_dγ)
-        I_P_dξ_dγ =
-            -F.J_P * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dγ) * Q + abs(Q)^2 * Q_dγ)
-
-        I_E_dξ_dϵ =
-            F.J_E_dϵ * abs(Q)^2σ * Q +
-            F.J_E * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dϵ) * Q + abs(Q)^2 * Q_dϵ)
-        I_P_dξ_dϵ =
-            -F.J_P_dϵ * abs(Q)^2σ * Q -
-            F.J_P * abs(Q)^(2σ - 2) * (2σ * real(conj(Q) * Q_dϵ) * Q + abs(Q)^2 * Q_dϵ)
-
-
-        dQ = γ * F.P_dξ + F.P * I_E_dξ + F.E_dξ * I_P + F.E * I_P_dξ
-        dQ_dγ = F.P_dξ + F.P * I_E_dξ_dγ + F.E * I_P_dξ_dγ + F.E_dξ * I_P_dγ
-        dQ_dϵ =
-            γ * F.P_dξ_dϵ +
-            F.P * I_E_dξ_dϵ +
-            F.P_dϵ * I_E_dξ +
-            F.E * I_P_dξ_dϵ +
-            F.E_dξ * I_P_dϵ +
-            F.E_dϵ * I_P_dξ +
-            F.E_dξ_dϵ * I_P
+        dQ = γ * F.P_dξ + F.E_dξ * I_P
+        dQ_dγ = F.P_dξ + F.E_dξ * I_P_dγ
+        dQ_dϵ = γ * F.P_dξ_dϵ + F.E_dξ * I_P_dϵ + F.E_dξ_dϵ * I_P
     end
 
     return SMatrix{2,2}(Q_dγ, dQ_dγ, Q_dϵ, dQ_dϵ)
